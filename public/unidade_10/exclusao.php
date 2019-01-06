9a45258cb80dceabd33b4080f851a8ee44c946ed
fabiovalinhos <fabiovalinhos@gmail.com>
2019-01-06 20:12:19 -0200

    Finish delete part and upload session

--- public/unidade_10/exclusao.php
+++ public/unidade_10/exclusao.php
@@ -1,5 +1,19 @@
 <?php require_once("../../conexao/conexao.php"); ?>
 <?php
+    if ( isset($_POST["nometransportadora"]) ) {
+        $tID = $_POST["transportadoraID"];
+
+        $exclusao = "DELETE FROM transportadoras ";
+        $exclusao .= "WHERE transportadoraID = {$tID}";
+        $con_exclusao = mysqli_query($conecta, $exclusao);
+
+        if(!$con_exclusao) {
+            die("Registro não excluído");
+        } else {
+            header("location: listagem.php");
+        }
+    }
+
     // Consulta a tabelas de transportadoras
     $tr = "SELECT * FROM transportadoras ";
     if (isset($_GET["codigo"])) {
@@ -7,14 +21,12 @@
         $tr .= "WHERE transportadoraID = {$id} ";
     }
     
-    $con_transportadora = myslqli_query($conecta, $tr);
-    if (!$con_transportadora) {
+    $con_transportadora = mysqli_query($conecta, $tr);
+    if(!$con_transportadora) {
         die("Erro na consulta");
     }
 
     $info_transportadora = mysqli_fetch_assoc($con_transportadora);
-    print_r($info_transportadora);
-    
 ?>
 
 <!doctype html>
@@ -25,13 +37,30 @@
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
+        <link rel="stylesheet" href="_css/alteracao.css">
     </head>
 
-    <body>
+    <body> 
         <?php include_once("_incluir/topo.php"); ?>
         
-        <main>  
-            
+        <main>
+            <div id="janela_formulario">
+                <form action="exclusao.php" method="post">
+                    <h2>Exclusão de Transportadoras</h2>
+                    
+                    <label for="nometransportadora">Nome da Transportadora</label>
+                    <input type="text" value="<?php echo utf8_encode($info_transportadora["nometransportadora"])  ?>" name="nometransportadora" id="nometransportadora">
+
+                    <label for="endereco">Endereço</label>
+                    <input type="text" value="<?php echo utf8_encode($info_transportadora["endereco"])  ?>" name="endereco" id="endereco">
+                    
+                    <label for="cidade">Cidade</label>
+                    <input type="text" value="<?php echo utf8_encode($info_transportadora["cidade"])  ?>" name="cidade" id="cidade">
+
+                    <input type="hidden" name="transportadoraID" value="<?php echo $info_transportadora["transportadoraID"] ?>">
+                    <input type="submit" value="Confirmar exclusão">                    
+                </form>   
+            </div>  
         </main>
 
         <?php include_once("_incluir/rodape.php"); ?>  
